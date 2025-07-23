6a633c21a91035e4f7b25c706af42d33155052ef
Albin Pious <albinpious65@gmail.com>
2025-07-23 16:53:18 +0530

    added simple interest calculator

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String  []args){
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        float r = sc.nextInt();
+        int t = sc.nextInt();
+        double pi = (p*r*t)/100;
+        System.out.println("simple interest caluculatd successfully. "+pi);
+    }
+}
